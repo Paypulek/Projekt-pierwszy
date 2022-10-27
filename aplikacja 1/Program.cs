74cb95c4a98e612e6ed00721ebfb66cc5da1f10d
Paypulek <116380893+Paypulek@users.noreply.github.com>
2022-10-27 17:32:53 +0200

    zadanie pierwsze

--- aplikacja 1/Program.cs
+++ aplikacja 1/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Wor2332432b43i2ld!");
-Console.ReadKey();
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace WitajSwiecie
+{
+    class Program
+    {
+        static void Main (string [] args)
+        { /* jakis komentarz */
+        Console.WriteLine("Witaj tutaj");
+        Console.ReadKey();
+        }
+    }
+}
