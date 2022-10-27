59dd350a8499f352a71d3c0473b7be4faf41537b
Paypulek <116380893+Paypulek@users.noreply.github.com>
2022-10-27 18:02:45 +0200

    zas nie dales komentarza

--- prostokat/Program.cs
+++ prostokat/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace PodstawowaSkladniaProstokąta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prostokat r = new Prostokat(); // pobaw sie i sprawdz co robi new
+            r.AkceptujDetale();
+            r.WyswietlCoWiesz();
+            Console.ReadKey(); // poczekam az nacisniesz guzik
+        }
+    }
+    //definicja klasyt prostokata
+    class Prostokat
+    {
+        //tworzymy zmienne
+        double a;
+        double b;
+        public void AkceptujDetale()
+        {
+            a = 20;
+            b = 45;
+        }
+        //liczenie powierzchnie
+        public double P()
+        {return a*b;
+        }
+        //Wyświetlenie danych
+        public void WyswietlCoWiesz()
+        {
+            Console.WriteLine("Długość: {0}", b);
+            Console.WriteLine("szerokosc: ", a);
+            Console.WriteLine("Pole:", P());
+        }
+    }
+}
