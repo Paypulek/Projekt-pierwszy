d8fbf36abd6a84cfba381d83a211d759f7a93cb0
Paypulek <116380893+Paypulek@users.noreply.github.com>
2022-10-27 18:42:46 +0200

    znów nie dałeś komentarza ale sprawdzarka do parzystych zrobiona

---
+++ liczbypierwsze/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+namespace Sprawdzarka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba;
+            int zmienna;
+            Console.WriteLine("Wprowadź liczbe, a odpowiem czy jest parzysta");
+liczba = Convert.ToInt32(Console.ReadLine());
+zmienna = liczba%2;
+if (zmienna == 0 )
+    {Console.WriteLine ("To jest parzysta");
+    }
+    else
+    {
+        Console.WriteLine ("to jest nieparzysta");
+        }
+    Console.ReadKey();
+
+        }
+    }
+}
