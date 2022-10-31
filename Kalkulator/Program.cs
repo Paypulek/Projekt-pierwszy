4fb6bbd00159d177fed2dcc9fd64c28b716fdd71
Paypulek <116380893+Paypulek@users.noreply.github.com>
2022-10-31 22:00:21 +0100

    Kalkulator

---
+++ Kalkulator/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Kalkulator;
+
+     class Program
+    {
+        static int Main(string[] args)
+        {
+
+            Console.WriteLine("Hej możesz tutaj wykonywać proste działania matematyczne");
+            bool czySukces;
+            char zerownik;
+            do
+            {
+
+                Console.WriteLine("wprowadź liczbę całkowitą na której chcesz wykonać operacje");
+                Console.WriteLine("a jak mnie nie będziesz słuchać to się wyłącze");
+
+                czySukces = int.TryParse(Console.ReadLine(), out int liczba);
+
+
+
+                if (czySukces)
+                {
+                    do
+                    {
+
+                        Console.WriteLine("wpisz znak działania jakie chcesz wykonać (/ * - +) z liczbą {0} ", liczba);
+
+                        czySukces = char.TryParse(Console.ReadLine(), out char znak);
+                        if (czySukces == false)
+                            return 0;
+
+                        Console.WriteLine(" wpisz sobie teraz liczbę drugą");
+
+                        czySukces = int.TryParse(Console.ReadLine(), out int liczba1);
+                        if (czySukces == false)
+                            return 0;
+
+
+                        switch (znak)
+                        {
+                            case '/':
+                                {
+                                    if (liczba1 == 0)
+                                    {
+                                        Console.WriteLine("Za te zero to też mi sie już nie chce działać nara");
+                                        return 0;
+                                    }
+
+                                    liczba = liczba / liczba1;
+                                    break;
+                                }
+
+                            case '*':
+                                liczba = liczba * liczba1;
+                                break;
+                            case '+':
+                                liczba = liczba + liczba1;
+                                break;
+                            case '-':
+                                liczba = liczba - liczba1;
+                                break;
+
+                        }
+                        Console.WriteLine("Wynik to {0}", liczba);
+                        Console.WriteLine("jak mam sie wyzerować to wpisz \"t\"");
+                        czySukces = char.TryParse(Console.ReadLine(), out zerownik);
+                        if (!czySukces)
+                            return 0;
+
+                        if (zerownik == 't')
+                            liczba = 0;
+
+
+
+                    } while (zerownik != 't');
+                }
+
+
+                else
+                {
+                    Console.WriteLine("Foch");
+                    return 0;
+                }
+
+            } while (czySukces);
+            return 0;
+        }
+    }
--- Wyszukiwarkaznaku/Program.cs
+++ Wyszukiwarkaznaku/Program.cs
@@ -7,12 +7,12 @@ namespace Wyszukiwarka
         static void Main(string[] args)
         {
             Console.WriteLine("Podaj zdanie a poszukam ile razy wystepuje tam literka \"a\"");
-            string zdanie = Console.ReadLine();
+            string zdanie = Console.ReadLine()!;
             int dlugosc = zdanie.Length;
             
             
               int ppow = 0;
-            for(int a = 0; a<= dlugosc; a++)
+            for(int a = 0; a< dlugosc; a++)
             {
                 if(zdanie[a]== 'a')
                 ppow++;
