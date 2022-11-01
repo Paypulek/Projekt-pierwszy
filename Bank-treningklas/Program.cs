a54696d604f9d6e861269c0684c20e3e0782bed3
Paypulek <116380893+Paypulek@users.noreply.github.com>
2022-11-01 17:47:43 +0100

    komentarz pisz od razu

---
+++ Bank-treningklas/Program.cs
@@ -0,0 +1,7 @@
+﻿Span<int> numbers = new int[] { 3, 14, 15, 92, 6 };
+foreach (int number in numbers)
+{
+    Console.Write($"{number} ");
+}
+// Output:
+// 3 14 15 92 6
---
+++ cwiczeniaTablice/Program.cs
@@ -0,0 +1,7 @@
+﻿var liczby = new List<int> {0,3,4,5,6,7,2,1,3,4};
+foreach (int wartosc in liczby)
+{
+   int wartosc1=1+wartosc;
+    Console.WriteLine($"wartosc {wartosc1} {wartosc}");
+
+}
