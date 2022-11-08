3dbb93a5bea2a5cf9ec993b92b745f0b647269c9
Paypulek <116380893+Paypulek@users.noreply.github.com>
2022-11-08 21:55:28 +0100

    MagazynRusztowania

---
+++ RusztowaniaMagazyn/Klient.cs
@@ -0,0 +1,17 @@
+class Klient
+{
+    string imie
+    {get;}
+    string nazwisko
+    {get;}
+    string miasto
+    {get;}
+
+    int stopy {get;set;}
+    int ramki {get;set;}
+    int podestyDrewniane {get;set;}
+    int podestKomunikacyjny {get;set;}
+    int stężenie {get;set;}
+    int barierki {get;set;}
+    int słupkiDoBarierek {get;set;}
+}
---
+++ RusztowaniaMagazyn/Magazyn.cs
@@ -0,0 +1,10 @@
+class Magazyn
+{
+    static int stopy {get;set;}
+    static int ramki {get;set;}
+    static int podestyDrewniane {get;set;}
+    static int podestKomunikacyjny {get;set;}
+    static int stężenie {get;set;}
+    static int barierki {get;set;}
+    static int słupkiDoBarierek {get;set;}
+}
