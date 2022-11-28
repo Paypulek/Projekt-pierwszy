d274c3828487f15070f636833a95a74cbc3d8b13
Paypulek <116380893+Paypulek@users.noreply.github.com>
2022-11-28 23:40:16 +0100

    Zapisprofilaktyczny

---
+++ Ilepaczekwtransporcie/StanAplikacji.cs
@@ -0,0 +1,6 @@
+public static class StanAplikacji
+{
+   public static List<StanPaczek> ListaPaczek;
+}
+
+
---
+++ Ilepaczekwtransporcie/StanPaczek.cs
@@ -0,0 +1,8 @@
+public class StanPaczek
+{
+        public Paczka Paczka { get; }
+        public int Stan { get;}
+        public StanPaczek(Paczka paczka, int stan) {this.Paczka=paczka; this.Stan=stan;}
+        
+
+}
