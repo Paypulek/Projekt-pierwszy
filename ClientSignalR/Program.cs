fb7c17d6c610ff13069e5a4a2f9c3e32a5d48c18
Paypulek <paypul0@gmail.com>
2023-06-07 19:39:44 +0200

    main

---
+++ ClientSignalR/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+var uri = "http://localhost:5000/chat";
+
+await using var connection = new HubConnectionBuilder()
+.WithUrl(uri)
+.Build();
+
+await connection.StartAsync();
+
+await foreach (var item in connection.StreamAsync<string>(methodName:"Streaming"))
+{
+    Console.WriteLine(item);
+}
---
+++ SignalR/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder => builder
+    .WithOrigins("http://localhost:59280")
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials());
+});
+
+var app = builder.Build();
+
+app.UseCors(policyName: "CorsPolicy");
+app.MapHub<MyHub>(pattern: "/chat");
+app.Run();
+
+public class MyHub : Hub
+{
+    public async IAsyncEnumerable<string> Streaming
+    (CancellationToken stoppingToken)
+    {
+          while (!stoppingToken.IsCancellationRequested)
+          {
+            yield return DateTime.UtcNow.ToString(format:"hh:mm:ss");
+
+            try
+            {
+                await Task.Delay(1000, stoppingToken);
+
+            }
+
+            catch (OperationCanceledException)
+            {
+                yield break;
+            }
+          }
+    }
+
+}
